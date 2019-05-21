44ae625cec08b21002df4c7a61bf4959bc83ad74
Craig Anderson <craig.anderson@wipro.com>
2019-05-21 07:36:39 +0100

    100% coverage

--- src/shExec.test.js
+++ src/shExec.test.js
@@ -1,22 +1,52 @@
 /* eslint-disable global-require */
 /* eslint-env mocha */
+const assert = require('assert');
 const td = require('testdouble');
 
 describe('shExec', () => {
   let exec;
   let shExec;
-  beforeEach(() => {
-    exec = td.function();
-    td.replace('shelljs', { exec });
-    shExec = require('./shExec');
+
+  describe('resolve', () => {
+    beforeEach(() => {
+      exec = td.function();
+      td.when(exec(td.matchers.isA(String), td.matchers.isA(Object))).thenCallback(0, 'stdout', 'stderr');
+      td.replace('shelljs', { exec });
+      shExec = require('./shExec');
+    });
+
+    afterEach(() => {
+      td.reset();
+    });
+
+    it('should call exec with the correct arguments', () => {
+      shExec('test', { cwd: 'cwd' });
+      td.verify(exec('test', { cwd: 'cwd' }, td.matchers.isA(Function)));
+    });
+
+    it('should resolve with stdout', async () => {
+      const out = await shExec('test', { cwd: 'cwd' });
+      assert.equal(out, 'stdout');
+    });
   });
 
-  afterEach(() => {
-    td.reset();
-  });
+  describe('reject', () => {
+    beforeEach(() => {
+      exec = td.function();
+      td.when(exec(td.matchers.isA(String), td.matchers.isA(Object))).thenCallback(1, 'stdout', 'stderr');
+      td.replace('shelljs', { exec });
+      shExec = require('./shExec');
+    });
 
-  it('should call exec with the correct arguments', () => {
-    shExec('test', { cwd: 'cwd' });
-    td.verify(exec('test', { cwd: 'cwd' }, td.matchers.isA(Function)));
+    afterEach(() => {
+      td.reset();
+    });
+    it('should reject with stderr', async () => {
+      try {
+        await shExec('test', { cwd: 'cwd' });
+      } catch (e) {
+        assert.equal(e, 'stderr');
+      }
+    });
   });
 });

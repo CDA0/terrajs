bf1c5f3443bef2b6079432dc19609c098ce9b0ba
Chris Forbes <git@chrisforbes.dev>
2019-11-18 16:50:58 +0000

    Add additional Handlebar helpers

--- src/registerHelpers.js
+++ src/registerHelpers.js
@@ -1,8 +1,22 @@
 const Handlebars = require('handlebars');
-const { camelToKebab, camelToSnake, includes } = require('./helpers');
+const {
+  camelToKebab,
+  camelToSnake,
+  includes,
+  ifeq,
+  ifneq,
+  switchHandlebarHelper,
+  caseHandlebarHelper,
+  defaultHandlebarHelper,
+} = require('./helpers');
 
 module.exports = () => {
   Handlebars.registerHelper('camelToKebab', camelToKebab);
   Handlebars.registerHelper('camelToSnake', camelToSnake);
   Handlebars.registerHelper('includes', includes);
+  Handlebars.registerHelper('ifeq', ifeq);
+  Handlebars.registerHelper('ifneq', ifneq);
+  Handlebars.registerHelper('switch', switchHandlebarHelper);
+  Handlebars.registerHelper('case', caseHandlebarHelper);
+  Handlebars.registerHelper('default', defaultHandlebarHelper);
 };

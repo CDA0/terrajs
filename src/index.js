65b45da9c0c0b9b724f1281e48bbcd3d697cf3a6
Chris Forbes <git@chrisforbes.dev>
2020-11-07 14:04:14 +0000

    Allow options to be passed down to shelljs

--- src/index.js
+++ src/index.js
@@ -40,94 +40,103 @@ class Terrajs {
     return tfCmd;
   }
 
-  async buildAndExec(action, args) {
+  async buildAndExec(action, args, execOptions) {
     const cmd = await this.buildCommand(action, args);
-    const opts = this.terraformDir ? { cwd: this.terraformDir } : {};
+    const opts = { ...execOptions };
+    if (this.terraformDir) {
+      opts.cwd = this.terraformDir;
+    }
     return shExec(cmd, opts);
   }
 
-  apply(args = {}) {
-    return this.execute ? this.buildAndExec('apply', args) : this.buildCommand('apply', args);
+  async commandWrapper(action, args, execOptions) {
+    return this.execute
+      ? this.buildAndExec(action, args, execOptions)
+      : this.buildCommand(action, args);
   }
 
-  destroy(args = {}) {
-    return this.execute ? this.buildAndExec('destroy', args) : this.buildCommand('destroy', args);
+  async apply(args = {}, execOptions = {}) {
+    return this.commandWrapper('apply', args, execOptions);
   }
 
-  fmt(args = {}) {
-    return this.execute ? this.buildAndExec('fmt', args) : this.buildCommand('fmt', args);
+  async destroy(args = {}, execOptions = {}) {
+    return this.commandWrapper('destroy', args, execOptions);
   }
 
-  get(args = {}) {
-    return this.execute ? this.buildAndExec('get', args) : this.buildCommand('get', args);
+  async fmt(args = {}, execOptions = {}) {
+    return this.commandWrapper('fmt', args, execOptions);
   }
 
-  graph(args = {}) {
-    return this.execute ? this.buildAndExec('graph', args) : this.buildCommand('graph', args);
+  async get(args = {}, execOptions = {}) {
+    return this.commandWrapper('get', args, execOptions);
   }
 
-  init(args = {}) {
-    return this.execute ? this.buildAndExec('init', args) : this.buildCommand('init', args);
+  async graph(args = {}, execOptions = {}) {
+    return this.commandWrapper('graph', args, execOptions);
   }
 
-  import(args = {}) {
-    return this.execute ? this.buildAndExec('import', args) : this.buildCommand('import', args);
+  async init(args = {}, execOptions = {}) {
+    return this.commandWrapper('init', args, execOptions);
   }
 
-  output(args = {}) {
-    return this.execute ? this.buildAndExec('output', args) : this.buildCommand('output', args);
+  async import(args = {}, execOptions = {}) {
+    return this.commandWrapper('import', args, execOptions);
   }
 
-  plan(args = {}) {
-    return this.execute ? this.buildAndExec('plan', args) : this.buildCommand('plan', args);
+  async output(args = {}, execOptions = {}) {
+    return this.commandWrapper('output', args, execOptions);
   }
 
-  providers(args = {}) {
-    return this.execute ? this.buildAndExec('providers', args) : this.buildCommand('providers', args);
+  async plan(args = {}, execOptions = {}) {
+    return this.commandWrapper('plan', args, execOptions);
   }
 
-  refresh(args = {}) {
-    return this.execute ? this.buildAndExec('refresh', args) : this.buildCommand('refresh', args);
+  async providers(args = {}, execOptions = {}) {
+    return this.commandWrapper('providers', args, execOptions);
   }
 
-  show(args = {}) {
-    return this.execute ? this.buildAndExec('show', args) : this.buildCommand('show', args);
+  async refresh(args = {}, execOptions = {}) {
+    return this.commandWrapper('refresh', args, execOptions);
   }
 
-  taint(args = {}) {
-    return this.execute ? this.buildAndExec('taint', args) : this.buildCommand('taint', args);
+  async show(args = {}, execOptions = {}) {
+    return this.commandWrapper('show', args, execOptions);
   }
 
-  untaint(args = {}) {
-    return this.execute ? this.buildAndExec('untaint', args) : this.buildCommand('untaint', args);
+  async taint(args = {}, execOptions = {}) {
+    return this.commandWrapper('taint', args, execOptions);
   }
 
-  validate(args = {}) {
-    return this.execute ? this.buildAndExec('validate', args) : this.buildCommand('validate', args);
+  async untaint(args = {}, execOptions = {}) {
+    return this.commandWrapper('untaint', args, execOptions);
   }
 
-  version(args = {}) {
-    return this.execute ? this.buildAndExec('version', args) : this.buildCommand('version', args);
+  async validate(args = {}, execOptions = {}) {
+    return this.commandWrapper('validate', args, execOptions);
   }
 
-  workspaceDelete(args = {}) {
-    return this.execute ? this.buildAndExec('workspace-delete', args) : this.buildCommand('workspace-delete', args);
+  async version(args = {}, execOptions = {}) {
+    return this.commandWrapper('version', args, execOptions);
   }
 
-  workspaceList(args = {}) {
-    return this.execute ? this.buildAndExec('workspace-list', args) : this.buildCommand('workspace-list', args);
+  async workspaceDelete(args = {}, execOptions = {}) {
+    return this.commandWrapper('workspace-delete', args, execOptions);
   }
 
-  workspaceNew(args = {}) {
-    return this.execute ? this.buildAndExec('workspace-new', args) : this.buildCommand('workspace-new', args);
+  async workspaceList(args = {}, execOptions = {}) {
+    return this.commandWrapper('workspace-list', args, execOptions);
   }
 
-  workspaceSelect(args = {}) {
-    return this.execute ? this.buildAndExec('workspace-select', args) : this.buildCommand('workspace-select', args);
+  async workspaceNew(args = {}, execOptions = {}) {
+    return this.commandWrapper('workspace-new', args, execOptions);
   }
 
-  workspaceShow(args = {}) {
-    return this.execute ? this.buildAndExec('workspace-show', args) : this.buildCommand('workspace-show', args);
+  async workspaceSelect(args = {}, execOptions = {}) {
+    return this.commandWrapper('workspace-select', args, execOptions);
+  }
+
+  async workspaceShow(args = {}, execOptions = {}) {
+    return this.commandWrapper('workspace-show', args, execOptions);
   }
 }
 

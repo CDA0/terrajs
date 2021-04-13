9f302631402806998ff2ec33c62a659949b02b27
Chris Forbes <git@chrisforbes.dev>
2021-04-13 21:20:10 +0100

    Remove switch/case/default helpers

--- src/helpers.js
+++ src/helpers.js
@@ -12,33 +12,6 @@ function ifneq(a, b, opts) {
   return (a !== b) ? opts.fn(this) : opts.inverse(this);
 }
 
-function switchHandlebarHelper(value, opts) {
-  this.switch_value = value;
-  this.switch_break = false;
-  return opts.fn(this);
-}
-
-function caseHandlebarHelper() {
-  /* eslint-disable-next-line prefer-rest-params */
-  const args = Array.prototype.slice.call(arguments);
-  const opts = args.pop();
-  const caseValues = args;
-
-  if (this.switch_break || caseValues.indexOf(this.switch_value) === -1) {
-    return '';
-  }
-
-  if (opts.hash.break === true) {
-    this.switch_break = true;
-  }
-
-  return opts.fn(this);
-}
-
-function defaultHandlebarHelper(opts) {
-  return (!this.switch_break ? opts.fn(this) : '');
-}
-
 function ifArray(value, opts) {
   return (value instanceof Array) ? opts.fn(this) : opts.inverse(this);
 }
@@ -53,9 +26,6 @@ module.exports = {
   includes,
   ifeq,
   ifneq,
-  switchHandlebarHelper,
-  caseHandlebarHelper,
-  defaultHandlebarHelper,
   ifArray,
   ifVersionSatisfies,
 };

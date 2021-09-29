2d1f0268cb0cb2ea5f683e50fdde2d2bfb42b671
William Saxton <s610062@cybg.com>
2021-09-29 14:26:00 +0100

    adding back in line breaks

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -3,18 +3,22 @@
     require('./0.11/destroy.test');
     require('./0.11/plan.test');
   });
+
   describe('Terraform v0.12.x', () => {
     require('./0.12/destroy.test');
     require('./0.12/plan.test');
   });
+
   describe('Terraform v0.13.x', () => {
     require('./0.13/destroy.test');
     require('./0.13/plan.test');
   });
+
   describe('Terraform v0.14.x', () => {
     require('./0.14/destroy.test');
     require('./0.14/plan.test');
   });
+
   describe('Terraform v0.15.x', () => {
     require('./0.15/init.test');
   });
